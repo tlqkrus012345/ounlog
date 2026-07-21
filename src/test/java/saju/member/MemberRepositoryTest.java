4ca1a5ae21d12936931a1d15daeb1c6b977509d8
tlqkrus012345 <tlqkrus012345@naver.com>
2026-07-21 20:28:38 +0900

    test: 회원 엔티티 저장

---
+++ src/test/java/saju/member/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package saju.member;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import saju.config.MysqlTestContainerConfig;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Import(MysqlTestContainerConfig.class)
+@DataJpaTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @DisplayName("회원 정보가 정상적으로 DB에 저장이 된다.")
+    @Test
+    void signup() {
+        Member member = Member.signup(
+                "email@test.com",
+                "password-hash"
+        );
+
+        Member savedMember = memberRepository.save(member);
+
+        assertThat(savedMember.getMemberId()).isNotNull();
+        assertThat(savedMember.getEmail()).as("email@test.com");
+    }
+}
