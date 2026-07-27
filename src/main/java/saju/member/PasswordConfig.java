80b3a8d1de410366d8b2344aab1022020b060887
tlqkrus012345 <tlqkrus012345@naver.com>
2026-07-27 22:18:05 +0900

    chore: 비밀번호 해쉬

---
+++ src/main/java/saju/member/PasswordConfig.java
@@ -0,0 +1,15 @@
+package saju.member;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
