fd999ceae1dcc825d38317798ba7c0369ad1aa48
tlqkrus012345 <tlqkrus012345@naver.com>
2026-08-17 17:08:33 +0900

    feat: 누락된 어노테이션

--- backend/src/main/java/saju/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/saju/common/exception/GlobalExceptionHandler.java
@@ -5,10 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
 @Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
