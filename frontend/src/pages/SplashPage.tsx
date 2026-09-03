c31504b01677d943464488df40aa52a841dbbd9a
tlqkrus012345 <tlqkrus012345@naver.com>
2026-09-03 13:30:22 +0900

    chore: 들여쓰기

--- frontend/src/pages/SplashPage.tsx
+++ frontend/src/pages/SplashPage.tsx
@@ -1,24 +1,24 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
 
 function SplashPage() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   useEffect(() => {
     const timerId = window.setTimeout(() => {
-      navigate("/entry", { replace: true });
-    }, 1000);
+      navigate('/entry', { replace: true })
+    }, 1000)
 
     return () => {
-      window.clearTimeout(timerId);
-    };
-  }, [navigate]);
+      window.clearTimeout(timerId)
+    }
+  }, [navigate])
 
   return (
     <main className="splash">
       <h1 className="splash__title">오늘 당신을 기다리는 운명의 메시지.</h1>
     </main>
-  );
+  )
 }
 
-export default SplashPage;
+export default SplashPage
