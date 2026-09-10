b2348668bfb69492cc4958a2a8da7c142fa8b4df
tlqkrus012345 <tlqkrus012345@naver.com>
2026-09-10 20:49:44 +0900

    feat(be): 로그인 실패 시 예외 동작 기능 구현

--- backend/src/main/java/ounlog/auth/service/AuthenticationService.java
+++ backend/src/main/java/ounlog/auth/service/AuthenticationService.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
+import ounlog.auth.exception.AuthErrorCode;
+import ounlog.auth.exception.AuthException;
 import ounlog.auth.jwt.JwtTokenProvider;
 import ounlog.auth.security.AuthenticatedMember;
 
@@ -16,8 +19,20 @@ public class AuthenticationService {
     private final JwtTokenProvider jwtTokenProvider;
 
     public LoginResult login(LoginCommand command) {
-        Authentication authentication = authenticationManager.authenticate(
-                UsernamePasswordAuthenticationToken.unauthenticated(command.email(), command.password()));
+        Authentication authentication;
+
+        try {
+            authentication = authenticationManager.authenticate(
+                    UsernamePasswordAuthenticationToken.unauthenticated(
+                            command.email(),
+                            command.password()
+                    )
+            );
+        } catch (AuthenticationException exception) {
+            throw new AuthException(
+                    AuthErrorCode.INVALID_CREDENTIALS
+            );
+        }
 
         AuthenticatedMember member = (AuthenticatedMember) authentication.getPrincipal();
 
