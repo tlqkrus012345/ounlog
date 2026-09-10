c0ebe17d5e9490c723e341031b2c96276ddb26d7
tlqkrus012345 <tlqkrus012345@naver.com>
2026-09-10 20:08:32 +0900

    feat(be): Spring Security 전용을 위한 인증 객체

---
+++ backend/src/main/java/ounlog/auth/security/AuthenticatedMember.java
@@ -0,0 +1,26 @@
+package ounlog.auth.security;
+
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ounlog.member.entity.Role;
+
+public record AuthenticatedMember(Long memberId, String email, String password, Role role) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+}
