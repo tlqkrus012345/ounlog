ca24875eb51e7445e84ef5aff0ca9b378cfbb09f
tlqkrus012345 <tlqkrus012345@naver.com>
2026-09-14 21:57:17 +0900

    chore(be): bcrypt password 빈 등록

--- backend/src/main/java/ounlog/auth/config/SecurityConfig.java
+++ backend/src/main/java/ounlog/auth/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -58,5 +59,10 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder);
 
         return new ProviderManager(provider);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
